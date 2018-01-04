7d5ce3047a68f97bcbf642ff0bca5291e8536b27
David Lars Ketch <davidlarsketch@gmail.com>
2018-01-03 20:14:00 -0600

    footer.js generates footer in each page

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const footer = () => {
+  const here = document.getElementById("footer");
+  const divElm = document.createElement("div");
+  divElm.className = "footer title";
+  const footerText = document.createTextNode("\u00a9 2017 \u00a0-\u00a0 DLK");
+  here.appendChild(divElm);
+  divElm.appendChild(footerText);
+};
+
+module.exports = footer;
