c49f59d6e44855a2a180bfb4e76ad5266974fffd
David Lars Ketch <davidlarsketch@gmail.com>
2018-01-09 13:48:39 -0600

    Updates header for using jQuery

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,38 +1,35 @@
 "use strict";
 
+const $ = require("jquery");
 const pages = ["Resume", "Projects", "Blog", "Contact"];
 
 const header = () => {
-  const here = document.getElementById("header");
   const nav = document.createElement("nav");
 
   const titleElm = document.createElement("span");
   const titleLink = document.createElement("a");
   titleLink.className = "header title";
   titleLink.href = document.title === "David Lars Ketch" ? "#top" : "index.html";
-  const titleText = document.createTextNode("DLK");
+  titleLink.append("DLK");
 
   const navLinks = document.createElement("span");
-  pages.forEach(page => navLinks.appendChild(makePageNav(page)));
+  pages.forEach(page => navLinks.append(makePageNav(page)));
 
-  here.appendChild(nav);
-  nav.appendChild(titleElm);
-  titleElm.appendChild(titleLink);
-  titleLink.appendChild(titleText);
-  nav.appendChild(navLinks);
+  $("#header").append(nav);
+  nav.append(titleElm);
+  titleElm.append(titleLink);
+  nav.append(navLinks);
 };
 
 const makePageNav = item => {
   let spanElm = document.createElement("span");
   let linkElm = document.createElement("a");
-  let textNode;
 
   spanElm.className = "nav-links";
   linkElm.href = document.title === item ? "#top" : `${item.toLowerCase()}.html`;
-  textNode = document.createTextNode(item);
 
-  linkElm.appendChild(textNode);
-  spanElm.appendChild(linkElm);
+  linkElm.append(item);
+  spanElm.append(linkElm);
 
   return spanElm;
 };
