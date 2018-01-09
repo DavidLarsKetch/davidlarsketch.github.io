e924ef1481dfa6f74a39b3cef89a32562de566ac
David Lars Ketch <davidlarsketch@gmail.com>
2018-01-09 12:52:55 -0600

    Changes over to jQuery & Firebase DB

---
+++ assets/js/xhr.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const $ = require('jquery');
+const fbURL = "https://davidlarsketch-8da73.firebaseio.com";
+
+
+module.exports.fbDataProcessor = data => {
+  let dataToSend = [];
+  let keys = Object.keys(data);
+  keys.forEach(key => {
+    data[key].id = key;
+    dataToSend.push(data[key]);
+  });
+  return dataToSend;
+};
+
+module.exports.getBlog = () => {
+  return new Promise(function(resolve, reject) {
+    $.ajax({
+      url: `${fbURL}/blog/entries.json`
+    })
+    .done(data => resolve(data))
+    .fail(err => reject(err));
+  });
+};
+
+module.exports.getContacts = () => {
+  return new Promise(function(resolve, reject) {
+    $.ajax({
+      url: `${fbURL}/contacts.json`
+    })
+    .done(data => resolve(data))
+    .fail(err => reject(err));
+  });
+};
