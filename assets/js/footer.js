cc298bc21814d520d3a7ec0cf2d1738141c5bda2
David Lars Ketch <davidlarsketch@gmail.com>
2018-01-09 13:48:24 -0600

    Updates footer for using jQuery

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,12 +1,12 @@
 "use strict";
 
+const $ = require('jquery');
+
 const footer = () => {
-  const here = document.getElementById("footer");
   const divElm = document.createElement("div");
   divElm.className = "footer title";
-  const footerText = document.createTextNode("\u00a9 2017 \u00a0-\u00a0 DLK");
-  here.appendChild(divElm);
-  divElm.appendChild(footerText);
+  divElm.append('\u00a9 2017 \u00a0-\u00a0 DLK');
+  $("#footer").append(divElm);
 };
 
 module.exports = footer;
