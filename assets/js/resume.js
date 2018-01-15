bcf84e4075b82f1678d6bdb674b32c2440c5cd11
David Lars Ketch <davidlarsketch@gmail.com>
2018-01-15 15:20:33 -0600

    Adds skeleton fo resume.js

---
+++ assets/js/resume.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const $ = require('jquery');
+const xhr = require('./xhr');
+const storage = require('./storage');
+
+let resumeData;
+
+const makeResume = data => {
+};
+
+module.exports.resume = () => {
+};
