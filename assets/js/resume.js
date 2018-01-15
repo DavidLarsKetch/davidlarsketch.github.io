83a5715826d3207da4771a410b8b4bd4fd673733
David Lars Ketch <davidlarsketch@gmail.com>
2018-01-15 15:21:11 -0600

    Defines makeResume func in resume.js

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -7,6 +7,27 @@ const storage = require('./storage');
 let resumeData;
 
 const makeResume = data => {
+  const mainElm = document.createElement("main");
+  mainElm.className = "resume-content";
+  const resumeTitle = document.createElement("h1");
+  resumeTitle.append("Resume");
+
+  const educationElm = document.createElement("section");
+  educationElm.id = "education";
+  educationElm.className = "education";
+  //Filter data to get those items with class of education, append to educationElm
+
+  const workElm = document.createElement("section");
+  workElm.id = "workExperience";
+  workElm.className = "work-experience";
+  //Filter data to get those items with class of work, append to workExperience
+
+  mainElm.append(resumeTitle);
+  mainElm.append(educationElm);
+  mainElm.append(workElm);
+
+  return mainElm;
+
 };
 
 module.exports.resume = () => {
