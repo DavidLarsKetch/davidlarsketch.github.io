a80b18ad607b99e670c7e8584f71d7346bab5d06
David Lars Ketch <davidlarsketch@gmail.com>
2018-01-18 07:28:53 -0600

    Removes DOM interaction & AJAX calling from main.js, splitting these into model.js & view.js to be executed by controller.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,5 @@
 "use strict";
 
-const footer = require("./footer");
-footer();
-const header = require("./header");
-header();
+const controller = require("./controller");
 
-const pages = {
-  blog: require("./blogger"),
-  contact: require("./contact"),
-  projects: require("./projects"),
-  resume: require("./resume")
-};
-
-const page = document.title.toLowerCase();
-const re = new RegExp(page);
-
-for (let key in pages) {
-  let result = re.test(key);
-  if (result) {
-    pages[key]();
-  }
-}
+controller.loadPage();
