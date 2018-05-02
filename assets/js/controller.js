38aa660d94085e02dbcf86dad80de3651e7ee4be
David Lars Ketch <davidlarsketch@gmail.com>
2018-05-02 09:20:13 -0500

    Checks Local Storage with new DB data before reprinting

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -12,10 +12,13 @@ const loadPage = () => {
   let localStorageData = retrieve(`${currentPage}Data`);
   if (localStorageData) printThisPage(currentPage, localStorageData);
 
+// Gets Firebase data to update Local Storage & DOM, if necessary
   getData(currentPage)
   .then(data => {
-    printThisPage(currentPage, data);
-    save(`${currentPage}Data`, data);
+    if (localStorageData !== data) {
+      printThisPage(currentPage, data);
+      save(`${currentPage}Data`, data);
+    }
   })
   .catch(err => console.log(err));
 };
